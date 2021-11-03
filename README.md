a481406da267d303e5c156f842c56778ca0788e9
seiten-dev <sunsp_7@yahoo.co.jp>
2021-11-03 21:59:39 +0900

    Hello Worldが出力されるver

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # angular-spring
 ## 環境
 
-
-
-
 ## Gradleプロジェクトの作成
 - [spring initializr](https://start.spring.io/)へアクセス
 - 以下の設定を行い「GENERATE」を押下する。記載がない箇所はデフォルトの設定を行う
@@ -17,3 +14,14 @@
     - Lombok
     - Spring Web
     - MySQL Driver
+- serverフォルダを作成し、上記でダウンロードしたファイルを移動する
+
+```sh
+# アプリ下のディレクトリにいることを確認
+pwd
+
+# 以下の出力であればOK。カレントディレクトリが別の場所の場合はアプリ下に移動すること
+/{任意のパス}/angular-spring
+
+mkdir server
+```
--- server/api-sample/src/main/java/com/example/apisample/ApiSampleApplication.java
+++ server/api-sample/src/main/java/com/example/apisample/ApiSampleApplication.java
@@ -3,9 +3,16 @@ package com.example.apisample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ApiSampleApplication {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController
+public class ApiSampleApplication {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSampleApplication.class, args);
 	}
